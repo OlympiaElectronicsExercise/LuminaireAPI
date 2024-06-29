2dc0f23ea091e4245e596f6e752fd052e1171cc8
George Kalleas <georgekalleas@outlook.com>
2024-06-29 18:27:21 +0300

    EmailSender

--- API/Extensions/ServicesConfigurationsExtensions.cs
+++ API/Extensions/ServicesConfigurationsExtensions.cs
@@ -1,15 +1,21 @@
 using API.Interfaces;
+using API.Models;
 using API.Services;
+using Microsoft.Extensions.Configuration;
 
 namespace API.Extensions
 {
     public static class ServicesConfigurationsExtensions
     {
-        public static IServiceCollection AddServices(this IServiceCollection services)
+        public static IServiceCollection AddServices(this IServiceCollection services, IConfiguration config)
         {
-            services.AddScoped<ITokenService, TokenService>(); 
-            // services.AddTransient<ITokenService, TokenService>();
-            // services.AddSingleton<ITokenService, TokenService>();
+            services.AddScoped<ITokenService, TokenService>();
+
+
+            var settings = config.GetSection("Email");
+            services.Configure<EmailConfigurationModel>(settings);
+            services.AddScoped<IEmailSender, EmailSender>();
+
             return services;
         }
     }
